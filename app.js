3ad44b7deb126ef1778f0f358bf9d5513d315e57
pooja suresh <Poojasurendran40@gmail.com>
2023-04-18 20:07:57 +0530

    Fix

--- app.js
+++ app.js
@@ -14,21 +14,23 @@ app.get("/" , function(req,res){
 
         app.post("/" , function(req,res){
                
-                const query = req.body.cityName;
+                let query = req.body.cityName;
 const appid = process.env.API_KEY;
 const units = "metric";
-const url = "https://api.openweathermap.org/data/2.5/weather?appid=process.env.API_KEY&q="+ query +"&units=metric";
+const url = "https://api.openweathermap.org/data/2.5/weather?&q="+ query +"&appid=" +appid+ "&units=metric";
 
 https.get(url,function(response){
     console.log(response);
    response.on("data" , function(data){
          const weatherData = JSON.parse(data);
 
-        const temp = weatherData.main.temp
-        const weatherDescription  = weatherData.weather[0].description
-        const icon = weatherData.weather[0].icon
-        const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
-        res.write("<h1>The Temperature in "+ query +" is " + temp + " degrees Celcius</h1>");
+        const temp = weatherData.main.temp;
+        const weatherDescription  = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+        query = query.toUpperCase();
+        res.write("<h1>The Temperature in "+ query +" is " + temp+ " degrees Celcius</h1>");
+        
         res.write("<p>The Weather is currently " +weatherDescription +"</p>");
         res.write("<img src="+imageURL+">");
         res.send();
